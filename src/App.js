368279b98c25b651c609977419d01dafc2d596fd
codedlab <sammyceejay@live.com>
2021-12-24 14:47:49 +0000

    completed sign in form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
-        <Route path='/shop' element={<ShopPage />}/>
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />}/>
       </Routes>
     </div>
   );
