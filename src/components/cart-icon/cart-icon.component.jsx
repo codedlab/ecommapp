cb685fe666e37f5c214a13ead49b7db190c39ee5
codedlab <sammyceejay@live.com>
2021-12-26 18:17:18 +0000

    updated code concerning reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'; 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
@@ -10,10 +11,8 @@ const CartIcon = ({toggleCartHidden, itemCount}) => (
     <span className='item-count'>{itemCount}</span>
   </div>
 );
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0
-  )
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
 })
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: ()=>dispatch(toggleCartHidden())
