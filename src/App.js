8bdf64f80f2bcc16f8581ed1c2a024bce219f655
codedlab <sammyceejay@live.com>
2021-12-28 22:57:20 +0000

    updated the checkout page component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { onSnapshot } from 'firebase/firestore';
@@ -54,6 +55,7 @@ class App extends React.Component {
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
+          <Route exact path='/checkout' element={<CheckoutPage/>} />
           <Route exact path='/signin' element={this.props.currentUser ? (<Navigate to='/'/>) : (<SignInAndSignUpPage />)} />
         </Routes>
       </div>
